f0730e62b01d467dfde895d8e86d03674db6eb4f
sravanth pasupuleti <sravanthpasupuleti@gmail.com>
2025-11-12 19:19:11 +0530

    updatee

---
+++ Strings/ReverseEachWord.java
@@ -0,0 +1,27 @@
+public class ReverseEachWord{
+    static String reversed = "";
+    static String method(String name){
+
+        int length = name.length() - 1;
+       
+        for(int i=0; i<=length; i++){
+            if(name.charAt(i) == ' ' || name.charAt(i) == length){
+                for(int j = i; j>=0; j--){
+                    if(name.charAt(j) == ' '){
+                        continue;
+                    }else{
+                        reversed = reversed + name.charAt(j);
+                    }
+                }
+                // reversed = reversed + " ";
+            }
+           
+        }
+
+        return reversed;
+    }
+    public static void main(String[] args){
+        System.out.println(ReverseEachWord.method("Hlo java Fine"));
+    }
+
+}
